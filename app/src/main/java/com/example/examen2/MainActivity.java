d3571744204bae778e0370d063422a6bcf93ea95
Ian SR <iann.ipn9@gmail.com>
2021-11-16 14:01:59 -0600

    cambio de listener

--- app/src/main/java/com/example/examen2/MainActivity.java
+++ app/src/main/java/com/example/examen2/MainActivity.java
@@ -22,12 +22,14 @@ public class MainActivity extends Activity implements View.OnTouchListener {
     private int x, y;
     private Lienzo l;
     TextView eventTV;
+    TextView patronTV;
     ArrayList<Integer> puntosUsados = new ArrayList<Integer>();
     ArrayList<Integer> patronGuardado = new ArrayList<Integer>();
     int[][] puntosCoordenadas = new int[9][2];
     Button saveBtn;
     Button accessBtn;
-    int col[]= new int[3];
+    int col[] = new int[3];
+    int row[] = new int[3];
 
     @Override
     protected void onCreate(Bundle b) {
@@ -37,33 +39,37 @@ public class MainActivity extends Activity implements View.OnTouchListener {
         DisplayMetrics displayMetrics = new DisplayMetrics();
         getWindowManager().getDefaultDisplay().getMetrics(displayMetrics);
         int width = displayMetrics.widthPixels;
-        col[0] = width/5;
-        col[1] = width/2;
-        col[2] = width*4/5;
+        int height = displayMetrics.heightPixels;
+        col[0] = width / 5;
+        col[1] = width / 2;
+        col[2] = width * 4 / 5;
+        row[0] = height * 2 / 5;
+        row[1] = height * 4 / 7;
+        row[2] = height * 26 / 35;
 
         //Fila 1
         puntosCoordenadas[0][0] = col[0];
         puntosCoordenadas[1][0] = col[1];
         puntosCoordenadas[2][0] = col[2];
-        puntosCoordenadas[0][1] = 1000;
-        puntosCoordenadas[1][1] = 1000;
-        puntosCoordenadas[2][1] = 1000;
+        puntosCoordenadas[0][1] = row[0];
+        puntosCoordenadas[1][1] = row[0];
+        puntosCoordenadas[2][1] = row[0];
 
         //Fila 2
         puntosCoordenadas[3][0] = col[0];
         puntosCoordenadas[4][0] = col[1];
         puntosCoordenadas[5][0] = col[2];
-        puntosCoordenadas[3][1] = 1400;
-        puntosCoordenadas[4][1] = 1400;
-        puntosCoordenadas[5][1] = 1400;
+        puntosCoordenadas[3][1] = row[1];
+        puntosCoordenadas[4][1] = row[1];
+        puntosCoordenadas[5][1] = row[1];
 
         //Fila 3
         puntosCoordenadas[6][0] = col[0];
         puntosCoordenadas[7][0] = col[1];
         puntosCoordenadas[8][0] = col[2];
-        puntosCoordenadas[6][1] = 1800;
-        puntosCoordenadas[7][1] = 1800;
-        puntosCoordenadas[8][1] = 1800;
+        puntosCoordenadas[6][1] = row[2];
+        puntosCoordenadas[7][1] = row[2];
+        puntosCoordenadas[8][1] = row[2];
 
         x = 100;
         y = 100;
@@ -72,36 +78,23 @@ public class MainActivity extends Activity implements View.OnTouchListener {
         l.setOnTouchListener(this);
         cl.addView(l);
         eventTV = (TextView) findViewById(R.id.eventTV);
+        patronTV = (TextView) findViewById(R.id.patronTV);
         saveBtn = (Button) findViewById(R.id.saveBtn);
         accessBtn = (Button) findViewById(R.id.accessBtn);
-        saveBtn.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                String str = "";
-                if (puntosUsados.size() > 1) {
-                    patronGuardado = puntosUsados;
-                    str = "Patron guardado!"+patronGuardado;
-                    eventTV.setText(patronGuardado+"");
-                } else {
-                    str = "No has pulsado nada";
-                }
-                puntosUsados.clear();
-                l.invalidate();
-                Toast t = Toast.makeText(getApplicationContext(), str, Toast.LENGTH_SHORT);
-                t.setGravity(Gravity.CENTER,0,0);
-                t.show();
-            }
-        });
+
+        saveBtn.setOnClickListener(guardarPatron);
+
         accessBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 boolean itsEqual = true;
+                patronTV.setText(patronGuardado + "");
                 String str = "";
                 if (patronGuardado.size() > 0) {
                     if (puntosUsados == patronGuardado) {
                         str = "Acediendo";
                     } else {
-                        str = "Los patrones no son iguales!\n"+patronGuardado+"\n"+puntosUsados;
+                        str = "Los patrones no son iguales!\n" + patronGuardado + "\n" + puntosUsados;
                         itsEqual = false;
                     }
                 } else {
@@ -111,7 +104,7 @@ public class MainActivity extends Activity implements View.OnTouchListener {
                 if (itsEqual) {
 //                    Intent i = new Intent(getApplicationContext(), acceso.class);
 //                    startActivity(i);
-                    str = "Es igual\n"+patronGuardado+"\n"+puntosUsados;
+                    str = "Es igual\n" + patronGuardado + "\n" + puntosUsados;
                 }
                 Toast t = Toast.makeText(getApplicationContext(), str, Toast.LENGTH_SHORT);
                 t.show();
@@ -120,6 +113,25 @@ public class MainActivity extends Activity implements View.OnTouchListener {
             }
         });
     }
+
+    public View.OnClickListener guardarPatron = new View.OnClickListener() {
+        @Override
+        public void onClick(View v) {
+            String str = "";
+            if (puntosUsados.size() > 1) {
+                patronGuardado = puntosUsados;
+                str = "Patron guardado!" + patronGuardado;
+                patronTV.setText(patronGuardado + "");
+            } else {
+                str = "No has pulsado nada";
+            }
+            puntosUsados.clear();
+            l.invalidate();
+            Toast t = Toast.makeText(getApplicationContext(), str, Toast.LENGTH_SHORT);
+            t.setGravity(Gravity.CENTER, 0, 0);
+            t.show();
+        }
+    };
 
     public boolean onTouch(View v, MotionEvent e) {
         x = (int) e.getX();
@@ -132,9 +144,9 @@ public class MainActivity extends Activity implements View.OnTouchListener {
             if (puntosUsados.size() == 1) {
                 puntosUsados.clear();
             }
-            if (puntosUsados.size()>=2){
-                x = getCoord(puntosUsados.get(puntosUsados.size()-1),0);
-                y = getCoord(puntosUsados.get(puntosUsados.size()-1),1);
+            if (puntosUsados.size() >= 2) {
+                x = getCoord(puntosUsados.get(puntosUsados.size() - 1), 0);
+                y = getCoord(puntosUsados.get(puntosUsados.size() - 1), 1);
                 eventTV.setText("");
             }
         }
@@ -145,43 +157,43 @@ public class MainActivity extends Activity implements View.OnTouchListener {
 
     private void stickyLine(int xPos, int yPos) {
         if (nearTo(xPos, col[0])) {
-            if (nearTo(yPos, 1000) && puntosUsados.indexOf(0) == -1) {
+            if (nearTo(yPos, row[0]) && puntosUsados.indexOf(0) == -1) {
                 eventTV.setText("Punto 1");
                 puntosUsados.add(0);
             }
-            if (nearTo(yPos, 1400) && puntosUsados.indexOf(3) == -1) {
+            if (nearTo(yPos, row[1]) && puntosUsados.indexOf(3) == -1) {
                 eventTV.setText("Punto 4");
                 puntosUsados.add(3);
             }
-            if (nearTo(yPos, 1800) && puntosUsados.indexOf(6) == -1) {
+            if (nearTo(yPos, row[2]) && puntosUsados.indexOf(6) == -1) {
                 eventTV.setText("Punto 7");
                 puntosUsados.add(6);
             }
         }
         if (nearTo(xPos, col[1])) {
-            if (nearTo(yPos, 1000) && puntosUsados.indexOf(1) == -1) {
+            if (nearTo(yPos, row[0]) && puntosUsados.indexOf(1) == -1) {
                 eventTV.setText("Punto 2");
                 puntosUsados.add(1);
             }
-            if (nearTo(yPos, 1400) && puntosUsados.indexOf(4) == -1) {
+            if (nearTo(yPos, row[1]) && puntosUsados.indexOf(4) == -1) {
                 eventTV.setText("Punto 5");
                 puntosUsados.add(4);
             }
-            if (nearTo(yPos, 1800) && puntosUsados.indexOf(7) == -1) {
+            if (nearTo(yPos, row[2]) && puntosUsados.indexOf(7) == -1) {
                 eventTV.setText("Punto 8");
                 puntosUsados.add(7);
             }
         }
         if (nearTo(xPos, col[2])) {
-            if (nearTo(yPos, 1000) && puntosUsados.indexOf(2) == -1) {
+            if (nearTo(yPos, row[0]) && puntosUsados.indexOf(2) == -1) {
                 eventTV.setText("Punto 3");
                 puntosUsados.add(2);
             }
-            if (nearTo(yPos, 1400) && puntosUsados.indexOf(5) == -1) {
+            if (nearTo(yPos, row[1]) && puntosUsados.indexOf(5) == -1) {
                 eventTV.setText("Punto 6");
                 puntosUsados.add(5);
             }
-            if (nearTo(yPos, 1800) && puntosUsados.indexOf(8) == -1) {
+            if (nearTo(yPos, row[2]) && puntosUsados.indexOf(8) == -1) {
                 eventTV.setText("Punto 9");
                 puntosUsados.add(8);
             }
@@ -189,7 +201,7 @@ public class MainActivity extends Activity implements View.OnTouchListener {
     }
 
     public boolean nearTo(int pos, int destino) {
-        int allowedRange = 30;
+        int allowedRange = 50;
         if (pos > destino - allowedRange && pos < destino + allowedRange) {
             return true;
         }
@@ -210,21 +222,21 @@ public class MainActivity extends Activity implements View.OnTouchListener {
             p.setARGB(255, 55, 200, 75);
             p.setStrokeWidth(10);
             p.setStyle(Paint.Style.STROKE);
-            c.drawCircle(puntosCoordenadas[0][0], puntosCoordenadas[0][1], 20, p);
-            c.drawCircle(puntosCoordenadas[1][0], puntosCoordenadas[1][1], 20, p);
-            c.drawCircle(puntosCoordenadas[2][0], puntosCoordenadas[2][1], 20, p);
+            c.drawCircle(puntosCoordenadas[0][0], puntosCoordenadas[0][1], 40, p);
+            c.drawCircle(puntosCoordenadas[1][0], puntosCoordenadas[1][1], 40, p);
+            c.drawCircle(puntosCoordenadas[2][0], puntosCoordenadas[2][1], 40, p);
 
-            c.drawCircle(puntosCoordenadas[3][0], puntosCoordenadas[3][1], 20, p);
-            c.drawCircle(puntosCoordenadas[4][0], puntosCoordenadas[4][1], 20, p);
-            c.drawCircle(puntosCoordenadas[5][0], puntosCoordenadas[5][1], 20, p);
+            c.drawCircle(puntosCoordenadas[3][0], puntosCoordenadas[3][1], 40, p);
+            c.drawCircle(puntosCoordenadas[4][0], puntosCoordenadas[4][1], 40, p);
+            c.drawCircle(puntosCoordenadas[5][0], puntosCoordenadas[5][1], 40, p);
 
-            c.drawCircle(puntosCoordenadas[6][0], puntosCoordenadas[6][1], 20, p);
-            c.drawCircle(puntosCoordenadas[7][0], puntosCoordenadas[7][1], 20, p);
-            c.drawCircle(puntosCoordenadas[8][0], puntosCoordenadas[8][1], 20, p);
+            c.drawCircle(puntosCoordenadas[6][0], puntosCoordenadas[6][1], 40, p);
+            c.drawCircle(puntosCoordenadas[7][0], puntosCoordenadas[7][1], 40, p);
+            c.drawCircle(puntosCoordenadas[8][0], puntosCoordenadas[8][1], 40, p);
 
-            if (puntosUsados.size() >=1) {
-                c.drawLine(getCoord(puntosUsados.get(puntosUsados.size()-1), 0), getCoord(puntosUsados.get(puntosUsados.size()-1), 1),
-                        x, y ,p);
+            if (puntosUsados.size() >= 1) {
+                c.drawLine(getCoord(puntosUsados.get(puntosUsados.size() - 1), 0), getCoord(puntosUsados.get(puntosUsados.size() - 1), 1),
+                        x, y, p);
                 if (puntosUsados.size() >= 2) {
                     for (int i = 1; i < puntosUsados.size(); i++) {
 
